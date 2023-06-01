afce5adbc72f3b8d23b6485f62b0cd30ca78010a
Luca Marcellino <s234262@studenti.polito.it>
2023-06-01 11:01:32 +0200

    change in small

--- Centralized_Baseline/centralized_baseline_Marcellino.py
+++ Centralized_Baseline/centralized_baseline_Marcellino.py
@@ -70,7 +70,7 @@ testloader = torch.utils.data.DataLoader(testset,
 if args.pre_trained == 0:
     net = ClientModel(device = device)
 elif args.pre_trained == 1:
-    net = timm.create_model('vit_base_patch16_224', pretrained=True)
+    net = timm.create_model('vit_small_patch16_224', pretrained=True)
     net.head = nn.Linear(net.head.in_features, 10)
 net.to(device)
 
