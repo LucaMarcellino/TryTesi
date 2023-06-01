8f7ae879df0a6003b2c44307626d9b8b6988f135
Luca Marcellino <s234262@studenti.polito.it>
2023-06-01 15:30:19 +0200

    try

--- FedSam/fedsam/models/cifar10/dataloader.py
+++ FedSam/fedsam/models/cifar10/dataloader.py
@@ -40,7 +40,8 @@ class ClientDataset(Dataset):
 
         if train:
             self.train_transform = transforms.Compose([
-                                        transforms.RandomCrop(IMAGE_SIZE, padding=4),
+                                        transforms.Resize((224,224)),
+                                        transforms.RandomCrop(224, padding=4),
                                         transforms.RandomHorizontalFlip(),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
@@ -51,6 +52,7 @@ class ClientDataset(Dataset):
         else:
             self.train_transform = None
             self.test_transform = transforms.Compose([
+                                        transforms.Resize((224,224)),
                                         transforms.ToTensor(),
                                         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                     ])
