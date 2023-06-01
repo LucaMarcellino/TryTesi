51a3cb5272c8db54ad832376d7901be3533d6170
Luca Marcellino <s234262@studenti.polito.it>
2023-06-01 10:34:49 +0200

    Changed args

---
+++ Centralized_Baseline/args.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument('--epochs',
+                        type=int,
+                        default=50)
+    parser.add_argument('--lr',
+                        type=float,
+                        default=0.01)
+    parser.add_argument('--opt',
+                        type=str,
+                        default='sgd')
+    parser.add_argument('--momentum',
+                        type=float,
+                        default=0.5)
+    parser.add_argument('--wd',
+                        type=float,
+                        default=0)
+    parser.add_argument('--batch_size',
+                        type=int,
+                        default=64)
+    parser.add_argument('pre_trained',
+                        type=int,
+                        help='0 no pretrained, 1 pretrained',
+                        default= 1)
+    
+    
+    return parser.parse_args()
--- Centralized_Baseline/centralized_baseline_Marcellino.py
+++ Centralized_Baseline/centralized_baseline_Marcellino.py
@@ -8,19 +8,24 @@ from reproducibility import make_it_reproducible,seed_worker
 from utils import get_datasets
 from tqdm import tqdm
 from ViT import ClientModel
+from args import parse_args
+import timm
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 seed = 0
 g = torch.Generator()
 
+
+
+
 #INPUT PARAMETERS
-epochs = 50
-norm_list = ["Batch Norm"]#, "Group Norm"]
-opt_list = ["sgd"]#,"adam"]
-lr_list = [0.001,0.01]
-momentum_list = [0.5,0.9]
-wd_list = [0]
+args = parse_args()
+opt = args.opt
+lr = args.lr
+momentum = args.momentum
+wd = args.wd
+epochs = args.epochs
 
 trainset,testset = get_datasets()
 criterion = nn.CrossEntropyLoss()
@@ -34,94 +39,96 @@ output_metrics["Train_Loss"] = list()
 output_metrics["Test_Loss"] = list()
 output_metrics["Test_Accuracy"] = list()
 
-for norm in norm_list:
-    for opt in opt_list:
-        for lr in lr_list:
-            for momentum in momentum_list:
-                for wd in tqdm(wd_list):
-
-                    print("Running test with:")
-                    print("Norm : {}".format(norm))
-                    print("Optimizer : {}".format(opt))
-                    print("Learning Rate : {}".format(lr))
-                    print("Momentum: {}".format(momentum))
-                    print("Weight_decay : {}".format(wd))
 
 
-                    make_it_reproducible(seed)
-                    g.manual_seed(seed)
 
-                    trainloader = torch.utils.data.DataLoader(trainset,
-                                                        batch_size = 64,
-                                                        shuffle = True,
-                                                        num_workers = 2,
-                                                        worker_init_fn = seed_worker,
-                                                        generator = g)
-                    testloader = torch.utils.data.DataLoader(testset,
-                                                    batch_size = 64,
-                                                    shuffle = False,
-                                                    num_workers = 2,
-                                                    worker_init_fn = seed_worker,
-                                                    generator = g)
 
-                    net = ClientModel(device = device)
-                    net.to(device)
+print("Running test with:")
+print("Optimizer : {}".format(opt))
+print("Learning Rate : {}".format(lr))
+print("Momentum: {}".format(momentum))
+print("Weight_decay : {}".format(wd))
 
-                    if opt == "sgd":
-                        optimizer = optim.SGD(net.parameters(), lr , momentum , wd )
-                    elif opt == "adam":
-                        optimizer = optim.Adam(net.parameters(), lr = lr , weight_decay = wd )
-                    
-                    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[20, 30, 40], gamma = 0.33)
 
-                    for epoch in tqdm(range(epochs)):
+make_it_reproducible(seed)
+g.manual_seed(seed)
 
-                        #TRAIN
-                        net.train()
-                        train_loss = []
-                        for batch_idx, (inputs, targets) in enumerate(trainloader):
-                            inputs, targets = inputs.to(device), targets.to(device)
-                            optimizer.zero_grad()
-                            outputs = net(inputs)
-                            loss = criterion(outputs, targets)
-                            loss.backward()
-                            optimizer.step()
-                            
-                            train_loss.append(loss.item())
-                        train_loss_avg = sum(train_loss) / len(train_loss)
+trainloader = torch.utils.data.DataLoader(trainset,
+                                    batch_size = args.batch_size,
+                                    shuffle = True,
+                                    num_workers = 2,
+                                    worker_init_fn = seed_worker,
+                                    generator = g)
+testloader = torch.utils.data.DataLoader(testset,
+                                batch_size = args.batch_size,
+                                shuffle = False,
+                                num_workers = 2,
+                                worker_init_fn = seed_worker,
+                                generator = g)
 
-                        #TEST
-                        net.eval()
-                        test_loss = []
-                        correct = 0
-                        total = 0
 
-                        with torch.no_grad():
-                            for batch_idx, (inputs, targets) in enumerate(testloader):
-                                inputs, targets = inputs.to(device), targets.to(device)
-                                outputs = net(inputs)
-                                loss = criterion(outputs, targets)
-                                _, predicted = outputs.max(1)
-                                total += targets.size(0)
-                                correct += predicted.eq(targets).sum().item()
-                                
-                                test_loss.append(loss.item())
-                                
-                            test_loss_avg = sum(test_loss) / len(test_loss)
-                            test_accuracy = correct / total
+if args.pre_trained == 0:
+    net = ClientModel(device = device)
+elif args.pre_trained == 1:
+    net = timm.create_model('vit_small_patch16_224', pretrained=True)
+    net.head = nn.Linear(net.head.in_features, 10)
+net.to(device)
 
-                        if epoch + 1 % 5 == 0:
-                            print("Epochs = {} | Train loss = {} | Test loss = {} | Test accuracy = {}".format(epoch + 1,train_loss_avg, test_loss_avg, test_accuracy))
-                        
-                        output_metrics["Epoch"].append(epoch + 1)
-                        output_metrics["Lr"].append(lr)
-                        output_metrics["Momentum"].append(momentum)
-                        output_metrics["Weight_decay"].append(wd)
-                        output_metrics["Train_Loss"].append(train_loss_avg)
-                        output_metrics["Test_Loss"].append(test_loss_avg)
-                        output_metrics["Test_Accuracy"].append(test_accuracy)
+if opt == "sgd":
+    optimizer = optim.SGD(net.parameters(), lr , momentum , wd )
+elif opt == "adam":
+    optimizer = optim.Adam(net.parameters(), lr = lr , weight_decay = wd )
 
-                        scheduler.step()
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[20, 30, 40], gamma = 0.33)
+
+for epoch in tqdm(range(epochs)):
+
+    #TRAIN
+    net.train()
+    train_loss = []
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+        
+        train_loss.append(loss.item())
+    train_loss_avg = sum(train_loss) / len(train_loss)
+
+    #TEST
+    net.eval()
+    test_loss = []
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+            
+            test_loss.append(loss.item())
+            
+        test_loss_avg = sum(test_loss) / len(test_loss)
+        test_accuracy = correct / total
+
+    if epoch + 1 % 5 == 0:
+        print("Epochs = {} | Train loss = {} | Test loss = {} | Test accuracy = {}".format(epoch + 1,train_loss_avg, test_loss_avg, test_accuracy))
+    
+    output_metrics["Epoch"].append(epoch + 1)
+    output_metrics["Lr"].append(lr)
+    output_metrics["Momentum"].append(momentum)
+    output_metrics["Weight_decay"].append(wd)
+    output_metrics["Train_Loss"].append(train_loss_avg)
+    output_metrics["Test_Loss"].append(test_loss_avg)
+    output_metrics["Test_Accuracy"].append(test_accuracy)
+
+    scheduler.step()
    
 
 
