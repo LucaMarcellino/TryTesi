85e088316a0a1b48fa484db5d65c1da3403e668d
Luca Marcellino <s234262@studenti.polito.it>
2023-07-03 10:34:12 +0200

    a

--- Centralized_Baseline/mycode.py
+++ Centralized_Baseline/mycode.py
@@ -104,7 +104,9 @@ print(first_image)
 net = ClientModel(device = device, pretrained=0, num_classes=10)
 optimizer = optim.SGD(net.parameters(), lr , momentum , wd )
 scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer, milestones=[20, 30, 40], gamma = 0.33)
-net.train()
+net.eval()
+output = net(first_image.unsqueeze[0])
+print(output) 
 
 
 
