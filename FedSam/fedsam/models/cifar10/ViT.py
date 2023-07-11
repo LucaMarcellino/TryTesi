33fc4d030ebb17927be6e5320472fbcd4d628852
Luca Marcellino <s234262@studenti.polito.it>
2023-07-11 17:21:23 +0200

    put vit small

--- FedSam/fedsam/models/cifar10/ViT.py
+++ FedSam/fedsam/models/cifar10/ViT.py
@@ -261,12 +261,12 @@ class Transformer(nn.Module):
 
 
 class ClientModel(nn.Module):
-    def __init__(self, device = None , config=configs.get_b16_config(), img_size=224, num_classes=10, zero_head=False, vis=False):
+    def __init__(self, device = None , config=configs.get_s16_config(), img_size=224, num_classes=10, zero_head=False, vis=False):
         super(ClientModel, self).__init__()
         self.num_classes = num_classes
         self.zero_head = zero_head
         self.classifier = config.classifier
-        self.transformer = timm.create_model('vit_base_patch16_224', pretrained=True)
+        self.transformer = timm.create_model('vit_small_patch16_224', pretrained=True)
         self.transformer.head = nn.Linear(self.transformer.head.in_features, num_classes)
         self.device = device
         self.size = self.model_size()
